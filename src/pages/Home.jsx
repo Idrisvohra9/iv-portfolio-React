db944a2cd232746b5e6f7984caa48144e8fe0378
Idrisvohra9 <idrishaider987@gmail.com>
2023-06-18 16:41:05 +0530

    New UI Stuff

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,13 +13,24 @@ export default function Header() {
   const { clientHeight } = document.documentElement;
 
   useEffect(() => {
+    // console.log();
     window.addEventListener("scroll", () => {
       let value = window.scrollY;
-      const heading = [...document.getElementsByClassName("heading")];
+      document
+        .querySelectorAll(".heading")
+        .forEach((head) => (head.style.top = 220 - value + "px"));
+      document
+        .querySelectorAll(".button")
+        .forEach((btn) => (btn.style.top = 350 - value + "px"));
 
-      if (heading) {
-        heading.forEach((head)=> head.style.top = 220 - value + "px");
-        document.getElementById("tentacles").style.left = -60 - value + "px";
+      // If small screen:
+      if (document.querySelector(".header-sm").checkVisibility()) {
+        document.querySelector(".layer1").style.top = -20 - value + "px";
+        document.querySelector(".layer2").style.top = -20 + value + "px";
+        document.querySelector(".moon").style.left = -40 + value + "px";
+        document.querySelector(".birds").style.left = 0 + value + "px";
+      } else {
+        document.querySelector(".tentacles").style.left = -60 - value + "px";
         document.querySelector(".turtle").style.top = 60 - value + "px";
         document.querySelector(".turtle").style.right = 210 - value + "px";
       }
@@ -35,54 +46,76 @@ export default function Header() {
         <title>Idris Vohra - Landing</title>
         <meta name="description" content="Landing Page for iv-portfolio" />
       </Helmet>
-      <header className="header-sm">
-        <section>
-          <div className="d-flex justify-content-center align-items-end h-100">
-            <p style={{ color: "#0f1b33", width: "75%" }}>
-              {" "}
-              <b>Note</b>: This page has parallax effect that can only be
-              experienced in screen with large size.
-            </p>
-          </div>
-        </section>
-        <section>
-          <h1 className="heading">Idris Vohra</h1>
-          <div className="d-flex justify-content-center">
-            <button className="button" onClick={scrollSmoothed}>
-              {" "}
-              Explore
-            </button>
-          </div>
-        </section>
-        <section></section>
+      <header className="header-sm" style={{ scrollBehavior: "smooth" }}>
+        <div className="starfall">
+          <span className="stars"></span>
+          <span className="stars"></span>
+          <span className="stars"></span>
+          <span className="stars"></span>
+          <span className="stars"></span>
+          <span className="stars"></span>
+          <span className="stars"></span>
+          <span className="stars"></span>
+          <span className="stars"></span>
+          <span className="stars"></span>
+        </div>
+
+        <div className="background2">
+          <img
+            src={require("./Images/layer1.png")}
+            alt="Parallax Image"
+            className="parallax-image img-fluid layer1"
+          />
+          <img
+            src={require("./Images/layer2.png")}
+            alt="Parallax Image"
+            className="parallax-image img-fluid layer2"
+          />
+          <img
+            src={require("./Images/moon.png")}
+            alt="Parallax Image"
+            className="parallax-image img-fluid moon"
+          />
+          <img
+            src={require("./Images/birds.png")}
+            alt="Parallax Image"
+            className="parallax-image img-fluid birds"
+          />
+        </div>
+        <h1 className="heading">Idris Vohra</h1>
+        <div className="d-flex justify-content-center">
+          <button className="button" onClick={scrollSmoothed}>
+            {" "}
+            Explore
+          </button>
+        </div>
         <div className="spacer"></div>
       </header>
       <header style={{ scrollBehavior: "smooth" }} className="header-lg">
         <img
           src={require("./Images/ColorBackground.webp")}
-          alt=""
+          alt="Parallax Image"
           className="parallax-image img-fluid background"
         />
         <img
           src={require("./Images/ForegroundReefs.webp")}
-          alt=""
+          alt="Parallax Image"
           className="parallax-image img-fluid reefs"
         />
         <img
           src={require("./Images/Fishes.webp")}
-          alt=""
+          alt="Parallax Image"
           className="parallax-image img-fluid fishes"
         />
         <img
           src={require("./Images/turtle.webp")}
-          alt=""
+          alt="Parallax Image"
           className="img-fluid turtle"
         />
         <img
           src={require("./Images/tentacles.webp")}
-          alt=""
+          alt="Parallax Image"
           className="parallax-image img-fluid tentacles"
-          id="tentacles"
         />
         <h1 className="heading">Idris Vohra</h1>
         <div className="d-flex justify-content-center">
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,20 +1,52 @@
 import { Outlet, NavLink } from "react-router-dom";
 // import { useRef } from "react";
-
+import React, { useEffect } from "react";
 const Layout = () => {
-  function showSideNav(e){
+  function showSideNav(e) {
     e.target.classList.toggle("clicked");
-    if(e.target.classList.contains("clicked")){
-      document.querySelector(".nav-scrollspy").style.display="block";
+    if (e.target.classList.contains("clicked")) {
+      document.querySelector(".nav-scrollspy").style.display = "block";
       // e.target.classList.remove("clicked");
-    }
-    else{
-      document.querySelector(".nav-scrollspy").style.display="none";
+    } else {
+      document.querySelector(".nav-scrollspy").style.display = "none";
     }
   }
+  var a_link = document.querySelectorAll("a");
+  useEffect(() => {
+    const cursor = document.getElementById("cursor");
+    // Handle mouse over/out event on links
+    a_link.forEach((e) => e.addEventListener("mouseenter", handleMouseEnter));
+    a_link.forEach((e) => e.addEventListener("mouseleave", handleMouseLeave));
+    window.addEventListener("mousemove", handleMouseMove);
+    // Move the cursor in dom/window
+    function handleMouseMove(event) {
+      var top = event.pageY - cursor.clientHeight / 2;
+      var left = event.pageX - cursor.clientWidth / 2;
+      cursor.style.top = top + "px";
+      cursor.style.left = left + "px";
+    }
+    // event: mouse enter on link
+    function handleMouseEnter(event) {
+      var _a = this;
+      var _a_width = _a.offsetWidth;
+      var classes = ["shape1", "shape2", "shape3", "shape4"];
+      var shape_class = classes[Math.floor(Math.random() * classes.length)];
+      // $('#cursor').css('width', _a_width+'px').addClass('hovered '+cls);
+      cursor.style.width = _a_width + "px";
+      cursor.classList.add("hovered", shape_class);
+    }
+    // event: mouse leave on link
+    function handleMouseLeave() {
+      cursor.style.width = "60px";
+      cursor.classList = "";
+    }
+  }, [a_link]);
   return (
     <>
       <div className="main-content">
+        <div id="cursor">
+          <div id="circle"></div>
+        </div>
         <i className="bi bi-list" onClick={showSideNav}></i>
         <div className="d-flex justify-content-center">
           <nav className="IV-nav">
